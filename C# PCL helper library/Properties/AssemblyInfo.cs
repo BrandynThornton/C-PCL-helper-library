aeb1f23e113c99808d3e2a198f46351f3b5e8fa5
BrandynThornton <brandyn.thornton@crownpeak.com>
2015-05-29 13:04:14 -0700

    updated gitignore from https://github.com/github/gitignore/blob/master/VisualStudio.gitignore, updated title and id for nuget package

--- C# PCL helper library/Properties/AssemblyInfo.cs
+++ C# PCL helper library/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CrownPeakPublic")]
+[assembly: AssemblyTitle("CrownPeak AccessAPI PCL")]
 [assembly: AssemblyDescription("A public project to help users make requests to Crownpeaks Access API")]
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyCompany("CrownPeak")]
-[assembly: AssemblyProduct("CrownPeakPublic")]
-[assembly: AssemblyCopyright("Copyright © CrownPeak 2014")]
+[assembly: AssemblyProduct("CrownPeakPublic.AccessAPI-PCL")]
+[assembly: AssemblyCopyright("Copyright © CrownPeak 2014, 2015")]
 [assembly: AssemblyTrademark("CrownPeak")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -27,5 +27,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.4")]
-[assembly: AssemblyFileVersion("1.0.0.4")]
+[assembly: AssemblyVersion("1.0.0.5")]
+[assembly: AssemblyFileVersion("1.0.0.5")]
